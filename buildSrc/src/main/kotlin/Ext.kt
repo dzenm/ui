b209ef193afba96e8c16068f4007742d20b87a4b
dzenm <dinzhenyan1997@126.com>
2021-06-13 11:12:10 +0800

    修复多模块引用产生的错误

--- buildSrc/src/main/kotlin/Ext.kt
+++ buildSrc/src/main/kotlin/Ext.kt
@@ -2,7 +2,7 @@ object Ext {
     const val compileSdkVersion = 30
     const val buildToolsVersion = "30.0.2"
     const val minSdkVersion = 21
-    const val targetSdkVersion = 29
+    const val targetSdkVersion = 30
     const val versionCode = 1
     const val versionName = "1.0"
 
